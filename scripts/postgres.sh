6d97f45db267ef1e76e5a87b9fade7517aed3724
Dirk Weil <dirk.weil@gedoplan.de>
2023-10-16 20:08:45 +0200

    .

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,1 +1,1 @@
-docker run -d --rm -p 5432:5432 -e POSTGRES_DB=showcase -e POSTGRES_USER=showcase -e POSTGRES_PASSWORD=showcase --name postgres_showcase postgres:14.2-alpine
+docker run -d --rm -p 5432:5432 -e POSTGRES_DB=showcase -e POSTGRES_USER=showcase -e POSTGRES_PASSWORD=showcase --name postgres_showcase postgres:16.0-alpine
